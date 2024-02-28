12cbcc93b5f3872e5e63120d65b86a1d04401a60
Raja Khan <getrk@yahoo.com>
2024-02-27 19:42:36 -0500

    Create create-products-plugin.php

---
+++ plugins/create-products/create-products-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Create Product Plugin
+Description: A Plugin to Define Products for an Ecommerce application.
+Version: 1.0
+Author: Raja Khan
+*/
+
+// Include necessary files
+require_once plugin_dir_path( __FILE__ ) . 'includes/create-product-form-handler.php';
